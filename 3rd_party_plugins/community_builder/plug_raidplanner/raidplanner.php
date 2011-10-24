58cf7a3a3199caa74e2076ebee4a4819b38d843f
Taracque <taracque@taracque.hu>
2011-10-24 13:29:55 +0200

    Added missing icons for User plugin

--- 3rd_party_plugins/community_builder/plug_raidplanner/raidplanner.php
+++ 3rd_party_plugins/community_builder/plug_raidplanner/raidplanner.php
@@ -13,7 +13,8 @@
 /** ensure this file is being included by a parent file */
 if ( ! ( defined( '_VALID_CB' ) || defined( '_JEXEC' ) || defined( '_VALID_MOS' ) ) ) { die( 'Direct Access to this location is not allowed.' ); }
 
-// register delete user code function
+global $_PLUGINS;
+
 $_PLUGINS->registerFunction( 'onAfterUserUpdate', 'syncRaidPlannerFields','getraidplannerTab' );
 
 /**
@@ -72,5 +73,129 @@ class getraidplannerTab extends cbTabHandler {
 			}
 		}
 	}
-	
+
 }
+
+$_PLUGINS->loadPluginGroup( 'user', array( (int) 1 ) );
+$_PLUGINS->registerUserFieldTypes( array( 'rpcharactersfield' => 'CBfield_rpcharacters' ) );
+$_PLUGINS->registerUserFieldParams();
+
+class CBfield_rpcharacters extends CBfield_textarea {
+	/**
+	 * Returns a field in specified format
+	 *
+	 * @param  moscomprofilerFields  $field
+	 * @param  moscomprofilerUser    $user
+	 * @param  string                $output  'html', 'xml', 'json', 'php', 'csvheader', 'csv', 'rss', 'fieldslist', 'htmledit'
+	 * @param  string                $reason  'profile' for user profile view, 'edit' for profile edit, 'register' for registration, 'list' for user-lists
+	 * @param  int                   $list_compare_types   IF reason == 'search' : 0 : simple 'is' search, 1 : advanced search with modes, 2 : simple 'any' search
+	 * @return mixed                
+	 */
+	function getField( &$field, &$user, $output, $reason, $list_compare_types ) {
+		$oReturn							=	'';
+
+		if ( is_object( $user ) ) {
+			switch ( $output ) {
+				case 'htmledit':
+					$oReturn =	null;
+					if ( $reason == 'edit' ) {
+						// Load the javascript
+						JHtml::_('behavior.framework');
+						JHtml::_('behavior.modal', 'a.modal');
+				
+						// Build the script.
+						$script = array();
+						$script[] = '';
+						$script[] = '	function jRecalCharacterValue_'.$field->fieldid.'() {';
+						$script[] = '		var ul = document.id("rp_characterEditorList_' . $field->fieldid . '");';
+						$script[] = '		var val = "";';
+						$script[] = '		ul.getChildren("li").each(function(li){';
+						$script[] = '			if (li.get("id") && (li.get("id") != "rp_characterEditorField_' . $field->fieldid . '_0") && (li.getChildren("a").get("text") != "") ) {';
+						$script[] = '				val = val + li.getChildren("a").get("text") + "\n";';
+						$script[] = '			}';
+						$script[] = '		})';
+						$script[] = '		document.id(rp_characterEditorValue_' . $field->fieldid . ').set("value", val);';
+						$script[] = '	}';
+						$script[] = '';
+						$script[] = '	function jSelectCharacter_'.$field->fieldid.'(idx, name) {';
+						$script[] = '		var line = document.id( "rp_characterEditorField_' . $field->fieldid . '_" + idx );';
+						$script[] = '		if (idx==0) {';
+						$script[] = '			var ul = line.getParent("ul");';
+						$script[] = '			ul.getChildren("li").each(function(li){';
+						$script[] = '				if ( (li) && (li.get("id")) ) {';
+						$script[] = '					if(li.get("id").replace("rp_characterEditorField_' . $field->fieldid . '_","")*1>idx) idx=li.get("id").replace("rp_characterEditorField_' . $field->fieldid . '_","")*1;';
+						$script[] = '					if(li.get("id") == "rp_characterEditorField_' . $field->fieldid . '_0") {';
+						$script[] = '						line = li.clone().setStyle("display","block");';
+						$script[] = '					}';
+						$script[] = '				}';
+						$script[] = '			})';
+						$script[] = '			idx = Number(idx) + 1;';
+						$script[] = '			line.set("id","rp_characterEditorField_' . $field->fieldid . '_" + (idx));';
+						$script[] = '			ul.grab(line,"top");';
+						$script[] = '			if (SqueezeBox) {';
+						$script[] = '				SqueezeBox.assign(line.getChildren("a"),{parse:"rel"});';
+						$script[] = '			}';
+						$script[] = '		}';
+						$script[] = '		line.getChildren("a").set("text",name);';
+						$script[] = '		line.getChildren("a").set("href","' . JURI::root() . 'index.php?option=com_raidplanner&amp;view=character&amp;layout=modal&amp;tmpl=component&amp;function=jSelectCharacter_'.$field->fieldid.'&amp;character=" + name + "&amp;fieldidx=" + idx );';
+						$script[] = '		line.getChildren("input").set("value",name)';
+						$script[] = '		SqueezeBox.close();';
+						$script[] = '		jRecalCharacterValue_'.$field->fieldid.'();';
+						$script[] = '	}';
+				
+						// Add the script to the document head.
+						JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
+				
+						$chars = str_replace( array("\n", ",", ";", "\r", "\t"), " ", $field->value );
+						$chars = preg_replace('!\s+!', ' ', $chars);
+						$chars = explode( " ", $chars);
+				
+						$html = '<input type="hidden" name="' . $field->name. '" value="' . implode("\n",$chars). '" id="rp_characterEditorValue_' . $field->fieldid . '" />';
+						$html .= '<div style="width:' . $field->params->get('cols', 80) . 'em;height:' . $field->params->get('rows',5) . 'em;overflow-y:auto;overflow-x:hidden;">';
+						$html .= '<ul style="display:block;float:left;clear:left;width:100%;padding:0;" id="rp_characterEditorList_' . $field->fieldid . '">';
+						$idx = 0;
+				
+						$html .= '<li style="display:none;float:left;clear:left;width:100%;" id="rp_characterEditorField_' . $field->fieldid . '_0">';
+						$html .= '<img src="' . JURI::root() . 'plugins/user/raidplanner/assets/delete.png" alt="' . JText::_('JACTION_DELETE') . '" onclick="this.getParent(\'li\').dispose();" style="float:right;margin:0;" />';
+						$html .= '<a class="modal" href="" rel="{handler: \'iframe\', size: {x: 450, y: 300}}"></a>';
+						$html .= '</li>';
+						
+						foreach ($chars as $char)
+						{
+							if ( trim($char) )
+							{
+								$idx ++;
+								$link = JURI::root() . 'index.php?option=com_raidplanner&amp;view=character&amp;layout=modal&amp;tmpl=component&amp;function=jSelectCharacter_'.$field->fieldid.'&amp;character=' . htmlspecialchars(trim($char), ENT_COMPAT, 'UTF-8') . '&amp;fieldidx=' . $idx;
+					
+								$html .= '<li style="display:block;float:left;clear:left;width:100%;" id="rp_characterEditorField_' . $field->fieldid . '_' . $idx . '">';
+								$html .= '<img src="' . JURI::root() . 'plugins/user/raidplanner/assets/delete.png" alt="' . JText::_('JACTION_DELETE') . '" onclick="this.getParent(\'li\').dispose();jRecalCharacterValue_'.$field->fieldid.'();" style="float:right;margin:0;" />';
+								$html .= '<a class="modal" href="' . $link . '" rel="{handler: \'iframe\', size: {x: 450, y: 300}}">' . $char . '</a>';
+								$html .= '</li>';
+							}
+						}
+						$link = JURI::root() . 'index.php?option=com_raidplanner&amp;view=character&amp;layout=modal&amp;tmpl=component&amp;function=jSelectCharacter_'.$field->fieldid.'&amp;character=&amp;fieldidx=';
+						$html .= '<li style="display:block;float:left;clear:left;width:100%;"><a class="modal" rel="{handler: \'iframe\', size: {x: 450, y: 300}}" href="' . $link . '"><img src="' . JURI::root() . 'plugins/user/raidplanner/assets/new.png" alt="' . JText::_('JACTION_NEW') . '" style="margin:0;" /> '. JText::_('PLG_USER_RAIDPLANNER_ADD_NEW_CHARACTER') . '</a></li>';
+				
+						$html .= '</ul>';
+						$html .= '</div>';
+						$oReturn = $html;
+					} else {
+						$oReturn = parent::getField( $field, $user, $output, $reason, $list_compare_types );
+					}
+					break;
+				case 'html':
+				case 'rss':
+				case 'json':
+				case 'php':
+				case 'xml':
+				case 'csvheader':
+				case 'fieldslist':
+				case 'csv':
+				default:
+					$oReturn = parent::getField( $field, $user, $output, $reason, $list_compare_types );
+					break;
+			}
+		}
+		return $oReturn;
+	}
+}//end of rank field
