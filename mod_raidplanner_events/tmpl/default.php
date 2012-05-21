68b2568d15e2bc65f825892e5fb803b7b90f2a16
Taracque <taracque@taracque.hu>
2012-05-21 09:04:46 +0200

    Revert 8aff357f92589f7a949303be3b61a6691b9e77a3, it causes problem (JDate cannot be converted to string)
    Needs more investigation.

--- mod_raidplanner_events/tmpl/default.php
+++ mod_raidplanner_events/tmpl/default.php
@@ -67,7 +67,7 @@ $lang->load('com_raidplanner');
 		<td>
 			<a href="<?php echo JRoute::_('index.php?option=com_raidplanner&view=calendar&task=default&modalevent='.$item->raid_id.'&Itemid='.$itemid);?>">
 				<span<?php if ($tip != '') { echo ' class="hasTip" title="'.$tip.'"'; } ?>>
-					<strong><?php echo JHTML::_('date', RaidPlannerHelper::getDate($item->start_time), RaidPlannerHelper::shortDateFormat() );?> </strong><?php echo $item->location;?>
+					<strong><?php echo JHTML::_('date', $item->start_time, RaidPlannerHelper::shortDateFormat() );?> </strong><?php echo $item->location;?>
 				</span>
 			</a><br />
 		</td>
