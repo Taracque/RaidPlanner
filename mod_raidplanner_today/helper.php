c56c7d0381c9665a351aef05db2c950b974acbf0
Taracque <taracque@taracque.hu>
2012-05-21 12:41:17 +0200

    Today module handles timezone again.

--- mod_raidplanner_today/helper.php
+++ mod_raidplanner_today/helper.php
@@ -25,14 +25,14 @@ class modRaidPlannerTodayHelper
 
 		// get a reference to the database
 		$db = &JFactory::getDBO();
-
+		
 		// get a list of raids ordered by start_time
-		$query = 'SELECT r.raid_id,r.location,r.start_time,s.confirmed,c.char_name,ro.role_name
+		$query = "SELECT r.raid_id,r.location,r.start_time,s.confirmed,c.char_name,ro.role_name
 					FROM `#__raidplanner_raid` AS r
-					LEFT JOIN `#__raidplanner_signups` AS s ON s.raid_id = r.raid_id AND s.profile_id = '.intval($user_id).' 
+					LEFT JOIN `#__raidplanner_signups` AS s ON s.raid_id = r.raid_id AND s.profile_id = ".intval($user_id)." 
 					LEFT JOIN `#__raidplanner_role` AS ro ON ro.role_id = s.role_id
 					LEFT JOIN `#__raidplanner_character` AS c ON c.character_id = s.character_id
-					WHERE DATE(start_time)=DATE(NOW()) ORDER BY location ASC';
+					WHERE DATE(DATE_ADD(start_time, INTERVAL " . intval(RaidPlannerHelper::getTimezone()) . " HOUR))=DATE(NOW()) ORDER BY location ASC";
 
 		$db->setQuery($query);
 		$items = ($items = $db->loadObjectList())?$items:array();
