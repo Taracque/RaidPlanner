f71ca0b01c6b146183bc7098ae5a318a65b45054
Taracque <taracque@taracque.hu>
2013-11-14 23:36:00 +0100

    Characters Bootstraped
    not tested…

--- administrator/com_raidplanner/views/characters/tmpl/default.php
+++ administrator/com_raidplanner/views/characters/tmpl/default.php
@@ -10,41 +10,51 @@
 -------------------------------------------------------------------------*/
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
+
+JHtml::_('bootstrap.tooltip');
+JHtml::_('formbehavior.chosen', 'select');
 ?>
-<form action="index.php" method="post" name="adminForm" id="adminForm">
-<div id="editcell">
-	<table>
-		<tr>
-			<td width="100%">
-				<?php echo JText::_( 'JSEARCH_FILTER_LABEL' ); ?>
-				<input type="text" name="search" id="search" value="<?php echo htmlspecialchars($this->lists['search']);?>" class="text_area" onchange="document.adminForm.submit();" />
-				<?php echo JText::_('COM_RAIDPLANNER_LEVEL'); ?>:
-				<input type="text" name="level_min" id="level_min" value="<?php echo htmlspecialchars($this->lists['level_min']);?>" class="text_area" onchange="document.adminForm.submit();" />
-				-
-				<input type="text" name="level_max" id="level_max" value="<?php echo htmlspecialchars($this->lists['level_max']);?>" class="text_area" onchange="document.adminForm.submit();" />
-				<?php echo JText::_( 'COM_RAIDPLANNER_GUILD' ); ?>
-				<select name="guild_filter" onchange="document.adminForm.submit();">
-					<option></option>
-					<?php foreach ($this->guilds as $guild_id => $guild): ?>
-					<option value="<?php echo $guild_id;?>" <?php if ($guild_id == $this->lists['guild_filter']) { echo "selected=\"selected\""; } ?>><?php echo $guild->guild_name;?></option>
-					<?php endforeach; ?>
-				</select>
-			</td>
-			<td nowrap="nowrap">
-				<button onclick="this.form.submit();"><?php echo JText::_( 'JSEARCH_FILTER_SUBMIT' ); ?></button>
-				<button onclick="document.getElementById('search').value='';document.getElementById('level_min').value='';document.getElementById('level_max').value='';this.form.submit();"><?php echo JText::_( 'JSEARCH_FILTER_CLEAR' ); ?></button>
-			</td>
-		</tr>
-	</table>
+<form action="<?php echo JRoute::_( 'index.php' );?>" method="post" name="adminForm" id="adminForm">
+	<div id="filter-bar" class="btn-toolbar">
+		<div class="filter-search btn-group pull-left">
+			<label for="search" class="element-invisible"><?php echo JText::_( 'JSEARCH_FILTER_LABEL' ); ?></label>
+			<input type="text" name="search" id="search" value="<?php echo htmlspecialchars($this->lists['search']);?>" class="text_area" onchange="document.adminForm.submit();" />
+		</div>
+		<div class="btn-group pull-left hidden-phone">
+			<label for="level_min" class="element-invisible"><?php echo JText::_( 'COM_RAIDPLANNER_LEVEL' ); ?></label>
+			<div class="input-append input-prepend">
+				<input type="text" name="level_min" id="level_min" value="<?php echo htmlspecialchars($this->lists['level_min']);?>" class="input-minimal" onchange="document.adminForm.submit();" />
+				<span class="add-on">-</span>
+				<input type="text" name="level_max" id="level_max" value="<?php echo htmlspecialchars($this->lists['level_max']);?>" class="input-minimal" onchange="document.adminForm.submit();" />
+			</div>
+			<label for="guild_filter" class="element-invisible"><?php echo JText::_( 'COM_RAIDPLANNER_GUILD' ); ?></label>
+			<select name="guild_filter" onchange="document.adminForm.submit();">
+				<option></option>
+				<?php foreach ($this->guilds as $guild_id => $guild): ?>
+				<option value="<?php echo $guild_id;?>" <?php if ($guild_id == $this->lists['guild_filter']) { echo "selected=\"selected\""; } ?>><?php echo $guild->guild_name;?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+		<div class="btn-group pull-left hidden-phone">
+			<button onclick="this.form.submit();" class="btn hasTooltip" title="" data-original-title="<?php echo JText::_( 'JSEARCH_FILTER_SUBMIT' ); ?>">
+				<i class="icon-search"></i>
+			</button>
+			<button onclick="document.getElementById('search').value='';document.getElementById('level_min').value='';document.getElementById('level_max').value='';this.form.submit();" class="btn hasTooltip" data-original-title="<?php echo JText::_( 'JSEARCH_FILTER_CLEAR' ); ?>">
+				<i class="icon-remove"></i>
+			</button>
+		</div>
+		<div class="btn-group pull-right hidden-phone">
+			<label for="limit" class="element-invisible"><?php echo JText::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
+			<?php echo $this->pagination->getLimitBox(); ?>
+		</div>
+	</div>
+	<div class="clearfix"> </div>
     <table class="adminlist table table-striped">
     <thead>
         <tr>
 			<th width="20">
 				<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count( $this->characters ); ?>);" />
 			</th>
-            <th width="5">
-                <?php echo JText::_( 'JGRID_HEADING_ID' ); ?>
-            </th>
             <th>
                 <?php echo JHTML::_( 'grid.sort', 'COM_RAIDPLANNER_CHARACTER_NAME', 'c.char_name', $this->lists['order_Dir'], $this->lists['order']); ?>
             </th>
@@ -69,6 +79,9 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
             <th>
                 <?php echo JHTML::_( 'grid.sort', 'COM_RAIDPLANNER_GUILD', 'g.guild_name', $this->lists['order_Dir'], $this->lists['order']); ?>
             </th>
+            <th width="5%">
+				<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'c.character_id', $this->lists['order_Dir'], $this->lists['order']); ?>
+            </th>
         </tr>            
     </thead>
     <tbody>
@@ -84,9 +97,6 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 			<td>
 				<?php echo $checked; ?>
 			</td>
-            <td>
-                <?php echo $row->character_id; ?>
-            </td>
             <td>
                 <a href="<?php echo JRoute::_('index.php?option=com_raidplanner&controller=characters&view=character&task=edit&cid[]='.$row->character_id);?>"><?php echo $row->char_name; ?></a>
             </td>
@@ -111,6 +121,9 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
             <td>
                 <a href="<?php echo JRoute::_('index.php?option=com_raidplanner&controller=guilds&view=guild&task=edit&cid[]='.$row->guild_id);?>"><?php echo $row->guild_name; ?></a>
             </td>
+            <td>
+                <?php echo $row->character_id; ?>
+            </td>
         </tr>
         <?php
         $k = 1 - $k;
@@ -123,12 +136,11 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 		</tr>
 	</tfoot>
     </table>
-</div>
-<input type="hidden" name="filter_order" value="<?php echo $this->lists['order']; ?>" />
-<input type="hidden" name="filter_order_Dir" value="<?php echo $this->lists['order_Dir']; ?>" />
-<input type="hidden" name="option" value="com_raidplanner" />
-<input type="hidden" name="task" value="" />
-<input type="hidden" name="boxchecked" value="0" />
-<input type="hidden" name="controller" value="characters" />
-<input type="hidden" name="view" value="characters" />
+	<input type="hidden" name="filter_order" value="<?php echo $this->lists['order']; ?>" />
+	<input type="hidden" name="filter_order_Dir" value="<?php echo $this->lists['order_Dir']; ?>" />
+	<input type="hidden" name="option" value="com_raidplanner" />
+	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="boxchecked" value="0" />
+	<input type="hidden" name="controller" value="characters" />
+	<input type="hidden" name="view" value="characters" />
 </form>
