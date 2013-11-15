91fc60fe53f470ae391a494b18b00dbaedcf1416
Taracque <taracque@taracque.hu>
2013-11-15 12:44:00 +0100

    Character edit form Bootstrapped

--- administrator/com_raidplanner/views/character/tmpl/form.php
+++ administrator/com_raidplanner/views/character/tmpl/form.php
@@ -12,63 +12,74 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
 
-<form action="index.php" method="post" name="adminForm" id="adminForm">
-<div class="width-100 fltlft col100">
+<form action="index.php" method="post" name="adminForm" id="adminForm" class="form-horizontal">
 	<fieldset class="adminform">
 		<legend><?php echo JText::_( 'JDETAILS' ); ?></legend>
-		<ul class="adminformlist">
-		<li>
-			<label for="char_name"><?php echo JText::_( 'COM_RAIDPLANNER_CHARACTER_NAME' ); ?>:</label>
-			<input class="text_area" type="text" name="char_name" id="char_name" size="32" maxlength="250" value="<?php echo $this->character->char_name;?>" />
-		</li>
-		<li>
-			<label for="profile_id"><?php echo JText::_( 'JGLOBAL_USERNAME' ); ?>:</label>
-			<select name="profile_id" id="profile_id">
-				<option value=""></option>
-				<?php foreach ($this->users as $user_id => $user) : ?>
-					<option value="<?php echo $user_id;?>"<?php if($user_id==@$this->character->profile_id){?> selected="selected"<?php } ?>><?php echo @$user->username;?></option>
-				<?php endforeach; ?>
-			</select>
-		</li>
-		<li>
-			<label for="class_id"><?php echo JText::_( 'COM_RAIDPLANNER_CLASS' ); ?>:</label>
-			<select name="class_id" id="class_id">
-				<?php foreach ($this->classes as $class_id => $class) : ?>
-					<option value="<?php echo $class_id;?>" style="color:<?php echo $class->class_color;?>"<?php if ($this->character->class_id == $class_id){ echo " selected=\"selected\"";}?>><?php echo $class->class_name;?></option>
-				<?php endforeach; ?>
-			</select>
-		</li>
-		<li>
-			<label for="gender_id"><?php echo JText::_( 'COM_RAIDPLANNER_GENDER' ); ?>:</label>
-			<?php echo JHTML::_('select.genericlist', $this->genders, 'gender_id', '', 'gender_id', 'gender_name', $this->character->gender_id); ?>
-		</li>
-		<li>
-			<label for="race_id"><?php echo JText::_( 'COM_RAIDPLANNER_RACE' ); ?>:</label>
-			<?php echo JHTML::_('select.genericlist', $this->races, 'race_id', '', 'race_id', 'race_name', $this->character->race_id); ?>
-		</li>
-		<li>
-			<label for="char_level"><?php echo JText::_( 'COM_RAIDPLANNER_LEVEL' ); ?>:</label>
-			<input class="text_area" type="text" name="char_level" id="char_level" size="10" maxlength="4" value="<?php echo $this->character->char_level;?>" />
-		</li>
-		<li>
-			<label for="rank"><?php echo JText::_( 'COM_RAIDPLANNER_RANK' ); ?>:</label>
-			<select name="rank" id="rank">
-				<?php foreach (RaidPlannerHelper::getRanks() as $rank_id => $rank) : ?>
-					<option value="<?php echo $rank_id;?>"<?php if ($this->character->rank == $rank_id){ echo " selected=\"selected\"";}?>><?php echo $rank;?></option>
-				<?php endforeach; ?>
-			</select>
-		</li>
-		<li>
-			<label for="guild_id"><?php echo JText::_( 'COM_RAIDPLANNER_GUILD' ); ?>:</label>
-			<?php echo JHTML::_('select.genericlist', $this->guilds, 'guild_id', '', 'guild_id', 'guild_name', $this->character->guild_id); ?>
-		</li>
-	</ul>
+		<div class="control-group">
+			<label class="control-label" for="char_name"><?php echo JText::_( 'COM_RAIDPLANNER_CHARACTER_NAME' ); ?></label>
+			<div class="controls">
+				<input class="text_area" type="text" name="char_name" id="char_name" size="32" maxlength="250" value="<?php echo $this->character->char_name;?>" />
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="profile_id"><?php echo JText::_( 'JGLOBAL_USERNAME' ); ?></label>
+			<div class="controls">
+				<select name="profile_id" id="profile_id">
+					<option value=""></option>
+					<?php foreach ($this->users as $user_id => $user) : ?>
+						<option value="<?php echo $user_id;?>"<?php if($user_id==@$this->character->profile_id){?> selected="selected"<?php } ?>><?php echo @$user->username;?></option>
+					<?php endforeach; ?>
+				</select>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="class_id"><?php echo JText::_( 'COM_RAIDPLANNER_CLASS' ); ?></label>
+			<div class="controls">
+				<select name="class_id" id="class_id">
+					<?php foreach ($this->classes as $class_id => $class) : ?>
+						<option value="<?php echo $class_id;?>" style="color:<?php echo $class->class_color;?>"<?php if ($this->character->class_id == $class_id){ echo " selected=\"selected\"";}?>><?php echo $class->class_name;?></option>
+					<?php endforeach; ?>
+				</select>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="gender_id"><?php echo JText::_( 'COM_RAIDPLANNER_GENDER' ); ?></label>
+			<div class="controls">
+				<?php echo JHTML::_('select.genericlist', $this->genders, 'gender_id', '', 'gender_id', 'gender_name', $this->character->gender_id); ?>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="race_id"><?php echo JText::_( 'COM_RAIDPLANNER_RACE' ); ?></label>
+			<div class="controls">
+				<?php echo JHTML::_('select.genericlist', $this->races, 'race_id', '', 'race_id', 'race_name', $this->character->race_id); ?>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="char_level"><?php echo JText::_( 'COM_RAIDPLANNER_LEVEL' ); ?></label>
+			<div class="controls">
+				<input class="text_area" type="text" name="char_level" id="char_level" size="10" maxlength="4" value="<?php echo $this->character->char_level;?>" class="input-mini" >
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="rank"><?php echo JText::_( 'COM_RAIDPLANNER_RANK' ); ?></label>
+			<div class="controls">
+				<select name="rank" id="rank">
+					<?php foreach (RaidPlannerHelper::getRanks() as $rank_id => $rank) : ?>
+						<option value="<?php echo $rank_id;?>"<?php if ($this->character->rank == $rank_id){ echo " selected=\"selected\"";}?>><?php echo $rank;?></option>
+					<?php endforeach; ?>
+				</select>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="guild_id"><?php echo JText::_( 'COM_RAIDPLANNER_GUILD' ); ?></label>
+			<div class="controls">
+				<?php echo JHTML::_('select.genericlist', $this->guilds, 'guild_id', '', 'guild_id', 'guild_name', $this->character->guild_id); ?>
+			</div>
+		</div>
 	</fieldset>
-</div>
-<div class="clr"></div>
-<input type="hidden" name="option" value="com_raidplanner" />
-<input type="hidden" name="character_id" value="<?php echo $this->character->character_id; ?>" />
-<input type="hidden" name="task" value="" />
-<input type="hidden" name="view" value="characters" />
-<input type="hidden" name="controller" value="characters" />
+	<input type="hidden" name="option" value="com_raidplanner" />
+	<input type="hidden" name="character_id" value="<?php echo $this->character->character_id; ?>" />
+	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="view" value="characters" />
+	<input type="hidden" name="controller" value="characters" />
 </form>
