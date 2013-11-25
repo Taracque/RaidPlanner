03b148ab9585db7f7a8a37fc9b810b5e46786162
Taracque <taracque@taracque.hu>
2013-11-25 16:07:39 +0100

    Race and Class css styles added to Joomla profile display mode.

--- plg_raidplanner/raidplanner.php
+++ plg_raidplanner/raidplanner.php
@@ -168,7 +168,7 @@ class plgUserRaidPlanner extends JPlugin
 			$chars = RaidPlannerHelper::getProfileChars( $value, true, true );
 			$ret = '';
 			foreach ($chars as $char) {
-				$ret .= $char['char_name'];
+				$ret .= '<span class="' . $char['class_css'] . ' ' . $char['race_css'] . '>' . $char['char_name'] . '<span>';
 				if ($char['guild_name']!='') {
 					$ret .= ' &lsaquo;' . $char['guild_name'] . '&rsaquo;';
 				}
