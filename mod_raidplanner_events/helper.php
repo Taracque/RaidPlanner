4e54abbcaf5c1d679d6439a48c199f539f13c0e9
Taracque <taracque@taracque.hu>
2013-11-27 00:46:46 +0100

    Same SQL Fix in mod_raidplanner_events module

--- mod_raidplanner_events/helper.php
+++ mod_raidplanner_events/helper.php
@@ -28,9 +28,8 @@ class modRaidPlannerEventsHelper
 		// get a list of $raidshow_number ordered by start_time
 		$query = "SELECT r.raid_id,r.location,r.start_time,s.confirmed,c.char_name,ro.role_name
 					FROM `#__raidplanner_raid` AS r
-					LEFT JOIN `#__raidplanner_signups` AS s ON s.raid_id = r.raid_id
+					LEFT JOIN (`#__raidplanner_signups` AS s,`#__raidplanner_character` AS c) ON (s.raid_id = r.raid_id AND c.character_id = s.character_id AND c.profile_id = " . intval($user_id) . ")
 					LEFT JOIN `#__raidplanner_role` AS ro ON ro.role_id = s.role_id
-					LEFT JOIN `#__raidplanner_character` AS c ON c.character_id = s.character_id AND c.profile_id = " . intval($user_id) . "
 					WHERE DATE(DATE_ADD(start_time, INTERVAL " . intval(RaidPlannerHelper::getTimezone()) . " HOUR))>=NOW() ORDER BY start_time ASC, location ASC LIMIT ".intval($raidshowNumber);
 
 		$db->setQuery($query);
