49cddfede6980e787933d488a097f68907a9e71a
Taracque <taracque@taracque.hu>
2013-11-29 00:36:17 +0100

    Rate_value field is signed

--- administrator/com_raidplanner/install/install.sql
+++ administrator/com_raidplanner/install/install.sql
@@ -115,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `#__raidplanner_rating` (
   `raid_id` int(10) unsigned NOT NULL DEFAULT '0',
   `character_id` int(10) unsigned NOT NULL DEFAULT '0',
   `rate_count` int(10) unsigned NOT NULL DEFAULT '0',
-  `rate_value` int(10) unsigned NOT NULL DEFAULT '0',
+  `rate_value` int(10) NOT NULL DEFAULT '0',
   `rated_by` text NOT NULL,
   PRIMARY KEY (`rating_id`),
   KEY `raid_id` (`raid_id`),
