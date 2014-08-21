5918bfad6577afd56f48b725b8ada460e3ada7d2
Taracque <taracque@taracque.hu>
2014-08-21 12:39:55 +0200

    Some more code for EasySocial

--- 3rd_party_plugins/easysocial/rp_characters/rp_characters.php
+++ 3rd_party_plugins/easysocial/rp_characters/rp_characters.php
@@ -1,3 +1,23 @@
 <?php
 
 defined('_JEXEC') or die('Restricted access');
+
+class SocialFieldsUserRP_Characters extends SocialFieldItem {
+
+	public function onRegister (&$post, &$registration) {
+		return $this->display();
+	}
+
+	public function onEdit (&$post, &$user) {
+		return $this->display();
+	}
+
+	public function onDisplay (&$post, &$user) {
+		return $this->display();
+	}
+
+	public function onSample () {
+		return $this->display();
+	}
+
+}
